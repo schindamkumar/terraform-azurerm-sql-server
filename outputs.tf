72d9e6b787e847914f930e65104295befcf95d6b
Dave Farinelli <d@dfar.io>
2019-09-30 18:52:19 -0400

    Added SQL Server admin password as output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "sqlserver_name" {
   value = "${azurerm_sql_server.sqlserver.name}"
 }
+
+output "sqlserver_admin_password" {
+  description = "Password for SQL Server admin user."
+  value = "${random_string.dbServerPassword.value}"
+}
