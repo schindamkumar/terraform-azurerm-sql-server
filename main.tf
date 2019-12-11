9ef88cba6ff5c21a2529f00b97a5274c59958f56
Dave Farinelli <d@dfar.io>
2019-12-11 13:15:50 -0500

    Removed deprecated interpolation

--- main.tf
+++ main.tf
@@ -4,19 +4,19 @@ resource "random_string" "dbServerPassword" {
 }
 
 resource "azurerm_sql_server" "sqlserver" {
-  name                         = "${lower(var.name)}"
-  resource_group_name          = "${var.rg_name}"
-  location                     = "${var.location}"
+  name                         = lower(var.name)
+  resource_group_name          = var.rg_name
+  location                     = var.location
   version                      = "12.0"
   administrator_login          = "sqladmin"
-  administrator_login_password = "${random_string.dbServerPassword.result}"
+  administrator_login_password = random_string.dbServerPassword.result
 }
 
 resource "azurerm_sql_database" "sqldatabase" {
-  count               = "${length(keys(var.databases))}"
-  name                = "${element(keys(var.databases), count.index)}"
-  resource_group_name = "${var.rg_name}"
-  location            = "${var.location}"
-  server_name         = "${var.name}"
-  edition             = "${element(values(var.databases), count.index)}"
+  count               = length(keys(var.databases))
+  name                = element(keys(var.databases), count.index)
+  resource_group_name = var.rg_name
+  location            = var.location
+  server_name         = var.name
+  edition             = element(values(var.databases), count.index)
 }
