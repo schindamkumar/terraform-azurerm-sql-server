5ce099318b228ca32180435b790a6b4f89dfe800
schindamkumar <42211249+schindamkumar@users.noreply.github.com>
2020-10-09 16:35:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  features {}
+} 
 resource "random_string" "dbServerPassword" {
   length  = 24
   special = false
